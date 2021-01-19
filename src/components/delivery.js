5ed915a1de383fecc000b62bd7f2439ac6c4935c
sanjeet <sanjeetky1999@gmail.com>
2021-01-19 23:51:41 +0530

    kjl

--- src/components/delivery.js
+++ src/components/delivery.js
@@ -1,7 +1,9 @@
 import React,{Component} from 'react';
 import { Breadcrumb, BreadcrumbItem,
-    Button, Row, Col, Label } from 'reactstrap';
+    Button, Row, Col, Label,Alert } from 'reactstrap';
     import { Link } from 'react-router-dom';
+    import { confirmAlert } from 'react-confirm-alert'; 
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
 
 
 import { Control, LocalForm, Errors } from 'react-redux-form';
@@ -15,6 +17,7 @@ constructor(props){
     this.state={
         item:[]
     }
+    this.delivered=this.delivered.bind(this);
 }
 componentDidMount()
 {
@@ -26,7 +29,41 @@ componentDidMount()
     .catch(err=>console.log(err))
 }
 
+delivered(data){
+    confirmAlert({
+        title: 'Confirm to submit',
+        message: 'Are you sure to do this.',
+        buttons: [
+          {
+            label: 'Yes',
+            onClick: () => {
+                const obj={
+                    id:data,
+                    status:"Delivered"
+                }
+               fetch( 'https://urbanreach.herokuapp.com/delivery',{
+                method:"PUT",
+                headers:{ "Content-Type":"application/json"},
+                body:JSON.stringify(obj)
+              
+            })
+            .then(data=>data.json())
+            .then(data=>alert("updated"))
+            .catch(err=>console.log(err))
+            }
+          },
+          {
+            label: 'No',
+            onClick: () => console.log("cancelled")
+          }
+        ]
+      });
+   
+
+
+}
   render() {
+    //  Alert(this.state.item)
       const items=this.state.item.map((data)=>{
            return(
                <div style={{marginBottom:30}}>
@@ -42,14 +79,19 @@ componentDidMount()
                {data.item.map((hello)=>{
                    return(
                        <div style={{marginBottom:10}}>
-                   <p style={{margin:0,padding:0}}>{hello.name}</p>
-                   <p style={{margin:0,padding:0}}>{hello.brand}</p>
-                   <p style={{margin:0,padding:0}}>{hello.weight}</p>
-                   <p style={{margin:0,padding:0}}>{hello.quantity}</p>
-
+                   <p style={{margin:0,padding:0}}>{hello.name} {hello.brand}</p>
+                   <p style={{margin:0,padding:0}}>{hello.weight} X {hello.quantity}</p>
+                   <p style={{margin:0,padding:0}}>Cost:{hello.cost}</p>
+                
                    </div>
                    )
                })}
+               
+               <p style={{margin:0,padding:0}}>Total Cost: {data.item.reduce((total,item)=>{return total+ (item.cost*item.quantity) },0)}</p>
+
+                   
+               <Button onClick={()=>this.delivered(data._id)} >Delivered</Button>
+
                <hr/>
                </div>
            )
