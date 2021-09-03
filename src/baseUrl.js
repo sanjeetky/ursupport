e9109f76e6fbbf71efb9306e5e015b22e5187b3e
sanjeet <sanjeetky1999@gmail.com>
2021-09-04 00:03:16 +0530

    kj

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,3 +1,3 @@
 
-//export const baseUrl="https://urbanreach.herokuapp.com";
-export const baseUrl="http://192.168.215.144:5050";
+export const baseUrl="https://urbanreach.herokuapp.com";
+//export const baseUrl="http://192.168.215.144:5050";
